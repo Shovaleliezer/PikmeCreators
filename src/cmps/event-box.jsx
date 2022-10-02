78fe4d5a8fdf15b404697c8139a041ad21154d2c
idan berger <idanberger22@gmail.com>
2022-10-02 22:37:57 +0300

    home css

--- src/cmps/event-box.jsx
+++ src/cmps/event-box.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom"
 export function EventBox(props) {
-    const {_id,banner,title} = props.ev
+    const {_id,banner,title,description} = props.ev
+    let desc = description.length>120? description.slice(0,120)+'...' : description
     return (
         <div className='event-preview clickable' >
             <NavLink to={`/event/${_id}`} className={`undecorate ${props.mode.type}`}>
                 <img src={banner} />
                 <h2>{title}</h2>
-                <p>watch the full game for only 5$</p>
+                <p>{desc}</p>
             </NavLink>
         </div>
     )
