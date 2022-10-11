867573d431ecf1b625b2b7731da1c3044d7d6b2e
Idan Berger <idanberger22@gmail.com>
2022-10-11 12:48:06 +0300

    feedback send emails

--- src/cmps/menu.jsx
+++ src/cmps/menu.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react"
 import { useSelector, useDispatch } from "react-redux"
+import emailjs from 'emailjs-com'
 import { toggleMode } from "../store/actions/general.actions"
 import { setMenu } from "../store/actions/general.actions"
 
@@ -12,9 +13,14 @@ export function Menu(props) {
     const { menu } = useSelector((storeState) => storeState.generalModule)
     let color = props.mode.type === 'light' ? '#1b1e1f' : '#f5f5f5'
 
-    const sendFeedback = (ev) => {
+    const sendFeedback = async(ev) => {
         ev.preventDefault()
         console.log({ txt: textRef.current.value, name:nameRef.current.value, email:mailRef.current.value})
+        emailjs.sendForm('service_6o4hbxh','template_wcfvzf6',ev.target,'72RBm-BgL2a--9Gky')
+        .then(() => {dispatch(setMenu('sent'))}
+         , (err) => {
+            console.log('FAILED...', err)
+         })
     }
 
     switch (menu) {
@@ -35,12 +41,12 @@ export function Menu(props) {
             return <>
                 <div className="screen" onClick={() => dispatch(setMenu(''))}></div>
                 <section className={`menu ${props.mode.type} noselect`}>
-                    <div className="close" onClick={() => dispatch(setMenu('normal'))}><span class="material-symbols-outlined">close</span></div>
+                    <div className="close" onClick={() => dispatch(setMenu('normal'))}><span className="material-symbols-outlined">close</span></div>
                     <form className="center-start" onSubmit={sendFeedback}>
                         <p>Feedback</p>
-                        <textarea rows="5" cols="25" className={props.mode.type} ref={textRef} autoFocus required placeholder="Please tell us how can we improve our product..."></textarea>
-                            <input type='text' ref={nameRef} className={`txt ${props.mode.type}`} placeholder='Your name' required/>
-                            <input type='email' ref={mailRef} className={`txt ${props.mode.type}`} placeholder='Your email' required/>
+                        <textarea name={'message'} rows="5" cols="25" className={props.mode.type} ref={textRef} autoFocus required placeholder="Please tell us how can we improve our product..."></textarea>
+                            <input name={'user_name'} type='text' ref={nameRef} className={`txt ${props.mode.type}`} placeholder='Your name' required/>
+                            <input name={'user_email'} type='email' ref={mailRef} className={`txt ${props.mode.type}`} placeholder='Your email' required/>
                         <div><input type="checkbox" id="notify" required ref={boxRef} />
                             <label htmlFor="notify"> Allow support to contact back</label></div>
                         <button className={`${props.mode.type} border-${props.mode.type}`}>Send</button>
@@ -51,20 +57,28 @@ export function Menu(props) {
             return <>
                 <div className="screen" onClick={() => dispatch(setMenu(''))}></div>
                 <section className={`menu ${props.mode.type} noselect`}>
-                    <div className="close" onClick={() => dispatch(setMenu('normal'))}><span class="material-symbols-outlined">close</span></div>
+                    <div className="close" onClick={() => dispatch(setMenu('normal'))}><span className="material-symbols-outlined">close</span></div>
                     <div className={`center-start ${props.mode.type}`}>
                         <p className="help-p">Help</p>
-                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span class="material-symbols-outlined">chevron_right</span></div>
-                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span class="material-symbols-outlined">chevron_right</span></div>
-                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to sdf dfsgdfg dfgd fgdg dgfdfg dfg rders?</p><span class="material-symbols-outlined">chevron_right</span></div>
-                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span class="material-symbols-outlined">chevron_right</span></div>
-                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span class="material-symbols-outlined">chevron_right</span></div>
-                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to sdf dfsgdfg dfgd fgdg dgfdfg dfg rders?</p><span class="material-symbols-outlined">chevron_right</span></div>
-                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span class="material-symbols-outlined">chevron_right</span></div>
-                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span class="material-symbols-outlined">chevron_right</span></div>
-                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to sdf dfsgdfg dfgd fgdg dgfdfg dfg rders?</p><span class="material-symbols-outlined">chevron_right</span></div>
+                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span className="material-symbols-outlined">chevron_right</span></div>
+                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span className="material-symbols-outlined">chevron_right</span></div>
+                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to sdf dfsgdfg dfgd fgdg dgfdfg dfg rders?</p><span className="material-symbols-outlined">chevron_right</span></div>
+                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span className="material-symbols-outlined">chevron_right</span></div>
+                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span className="material-symbols-outlined">chevron_right</span></div>
+                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to sdf dfsgdfg dfgd fgdg dgfdfg dfg rders?</p><span className="material-symbols-outlined">chevron_right</span></div>
+                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span className="material-symbols-outlined">chevron_right</span></div>
+                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to recieve reminders?</p><span className="material-symbols-outlined">chevron_right</span></div>
+                        <div className={`help-opt border-${props.mode.type} hover-main`}><p>how to sdf dfsgdfg dfgd fgdg dgfdfg dfg rders?</p><span className="material-symbols-outlined">chevron_right</span></div>
                     </div>
                 </section>
             </>
+        case 'sent':
+            return<>
+            <div className="screen" onClick={() => dispatch(setMenu(''))}></div>
+                <section className={`menu ${props.mode.type} noselect`}>
+                    <div className="close" onClick={() => dispatch(setMenu('normal'))}><span className="material-symbols-outlined">close</span></div>
+                    <p>We got you, our support team are doing their best to improve.</p>
+                </section>
+            </>    
     }
 }
