5f4e5d76ee481dc87513090d561b7df127cda8c1
idan berger <idanberger22@gmail.com>
2022-10-26 18:37:55 +0300

    grid view <5 in progress

--- src/cmps/event-box.jsx
+++ src/cmps/event-box.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from "react-router-dom"
+ 
 export function EventBox(props) {
     const {_id,banner,title,description} = props.ev
     let desc = description.length>120? description.slice(0,120)+'...' : description
+
     return (
         <div className='event-preview clickable' >
             <NavLink to={`/event/${_id}`} className={`undecorate ${props.mode.type}`}>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,7 @@ import { eventService } from '../services/eventService'
 import { setFilter } from '../store/actions/general.actions'
 import { EventBox } from '../cmps/event-box'
 import { FilterBar } from '../cmps/filter-bar'
+import { isMobile } from 'react-device-detect'
 
 export function Home(props) {
     const dispatch = useDispatch()
@@ -15,10 +16,23 @@ export function Home(props) {
         loadEvents(filter)
     }, [filter])
 
-
     const loadEvents = async (filter) => {
         const loadedEvents = await eventService.query(filter)
         setEvents(loadedEvents.slice(0, 4))
+    }
+
+    useEffect(() => {
+        window.addEventListener("wheel", e => handleScrolling(e), { passive: false })
+        return window.removeEventListener("wheel",e => handleScrolling(e))
+    }, [])
+
+    const handleScrolling = (e) => {
+        e.preventDefault()
+        // if (e.wheelDeltaY > 0) console.log('up')
+        // else console.log('down')
+
+        const element = document.getElementsByClassName("event-preview")[0]
+        element.scrollIntoView({alignToTop:true})
     }
 
     if (!events) return <p>loading...</p>
@@ -27,11 +41,11 @@ export function Home(props) {
         <div className={props.mode.type}>
             <FilterBar mode={props.mode} />
             {events.length >= 5 && <section className='events-container' style={{ gridTemplateColumns: `repeat(auto-fill, minmax(${gridView}, 1fr))` }}>
-                {events.map(ev => <EventBox ev={ev} mode={props.mode} key={ev._id} />)}
+                {events.map(ev => <EventBox margin={false} ev={ev} mode={props.mode} key={ev._id} />)}
             </section>}
 
-            {(events.length < 5 && events.length > 0) && <section className='container-few'>
-            {events.map(ev => <EventBox ev={ev} mode={props.mode} key={ev._id} />)}
+            {(events.length < 5 && events.length > 0) && <section className='container-few' scroll="no">
+                {events.map((ev,idx) => <EventBox margin={true} ev={ev} mode={props.mode} key={ev._id}/>)}
             </section>}
 
             {events.length === 0 && <div className="center not-found">
