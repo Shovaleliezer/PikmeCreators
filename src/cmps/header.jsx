77fa95b2013e3922afeacae8660dde826d2e4d7b
idan berger <idanberger22@gmail.com>
2022-10-30 14:13:43 +0200

    header-mobile

--- src/cmps/header.jsx
+++ src/cmps/header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { NavLink } from "react-router-dom"
 import { SearchBar } from "./search-bar"
@@ -8,8 +9,9 @@ import { useNavigate } from "react-router-dom"
 export function Header(props) {
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const [headerMode, setHeaderMode] = useState('')
     const user = useSelector((state) => state.user)
-    let isMobile = window.innerWidth < 980 ? true : false
+    let isMobile = window.innerWidth < 930 ? true : false
 
     const resetFilter = () => {
         dispatch(setFilter(''))
@@ -22,27 +24,47 @@ export function Header(props) {
 
     return (
         <>
-            <nav className={`header ${props.mode.type} noselect`}>
+            {!isMobile && <nav className={`header ${props.mode.type} noselect`}>
                 <NavLink onClick={resetFilter} className={`undecorate ${props.mode.type} hover-main`} to='/'><img className="logo" src={require('../style/imgs/logo.png')} /></NavLink>
 
                 <SearchBar mode={props.mode} />
 
                 <div className="left-bar">
-                    {!isMobile && <>
-                        <img onClick={() => { filterClick('valorant') }} className="bar-logo" src={require('../style/imgs/valorant-logo.png')} />
-                        <img onClick={() => { filterClick('fifa') }} className="bar-logo fifa" src={require('../style/imgs/fifa-logo.png')} />
-                        <img onClick={() => { filterClick('sport') }} className="bar-logo" src={require('../style/imgs/sport-logo.png')} />
-                        <NavLink to="/register"><img title="watch stream" className="bar-logo" src={require(`../style/imgs/stream-icon-${props.mode.type}.png`)} /></NavLink>
-                        <NavLink to='/profile'><img className='header-user-img' src={(user && user.image) ? user.image : require('../style/imgs/user-icon.png')} /></NavLink></>}
-                    <span className="material-symbols-outlined icons clickable hover-main" onClick={() => dispatch(toggleMenu())}>menu</span>
+                    <img onClick={() => { filterClick('valorant') }} className="bar-logo" src={require('../style/imgs/valorant-logo.png')} />
+                    <img onClick={() => { filterClick('fifa') }} className="bar-logo fifa" src={require('../style/imgs/fifa-logo.png')} />
+                    <img onClick={() => { filterClick('sport') }} className="bar-logo" src={require('../style/imgs/sport-logo.png')} />
+                    <NavLink to="/register"><img title="watch stream" className="bar-logo" src={require(`../style/imgs/stream-icon-${props.mode.type}.png`)} /></NavLink>
+                    <NavLink to='/profile'><img className='header-user-img' src={(user && user.image) ? user.image : require('../style/imgs/user-icon.png')} /></NavLink>
+                    <span className="material-symbols-outlined menu-icon clickable hover-main" onClick={() => dispatch(toggleMenu())}>menu</span>
                 </div>
-            </nav>
-
-            {isMobile && <nav className={`footer-mobile ${props.mode.type}`}>
-                <NavLink onClick={resetFilter} to='/'><img className='home-icon' src={require(`../style/imgs/home-icon-${props.mode.type}.png`)} /></NavLink>
-                <NavLink onClick={resetFilter} to='/'><img src={require(`../style/imgs/stream-icon-${props.mode.type}.png`)} /></NavLink>
-                <NavLink to='/profile'><img className='circle' src={(user && user.image) ? user.image : require('../style/imgs/user-icon.png')} /></NavLink>
             </nav>}
+
+            {isMobile && <>
+                <nav className={`header ${props.mode.type} noselect`}>
+                    {headerMode === 'search' ? <div className="mobile-search-wrapper">
+                        <SearchBar mode={props.mode} />
+                        <span onClick={()=>{setHeaderMode('')}} className="material-symbols-outlined">expand_less</span>
+                        </div> : <>
+                        <NavLink onClick={resetFilter} className={`undecorate ${props.mode.type} hover-main`} to='/'><img className="logo" src={require('../style/imgs/logo.png')} /></NavLink>
+                        <div className="header-mobile-bar">
+                            <span className="material-symbols-outlined search-icon" onClick={() => setHeaderMode('search')}>search</span>
+                            <img onClick={() => { setHeaderMode('games-bar') }} className="games-menu" src={require('../style/imgs/games-menu.png')} />
+                            <span className="material-symbols-outlined menu-icon clickable hover-main" onClick={() => dispatch(toggleMenu())}>menu</span>
+                        </div>
+                        {headerMode === 'games-bar' && <div className={'games-bar' + ' ' + props.mode.type}>
+                            <img onClick={() => { filterClick('valorant') }} className="bar-logo" src={require('../style/imgs/valorant-logo.png')} />
+                            <img onClick={() => { filterClick('fifa') }} className="bar-logo fifa" src={require('../style/imgs/fifa-logo.png')} />
+                            <img onClick={() => { filterClick('sport') }} className="bar-logo" src={require('../style/imgs/sport-logo.png')} />
+                            <span onClick={()=>{setHeaderMode('')}} className="material-symbols-outlined">expand_less</span>
+                        </div>}
+                    </>}
+                </nav>
+
+                <nav className={`footer-mobile ${props.mode.type}`}>
+                    <NavLink onClick={resetFilter} to='/'><img className='home-icon' src={require(`../style/imgs/home-icon-${props.mode.type}.png`)} /></NavLink>
+                    <NavLink onClick={resetFilter} to='/'><img src={require(`../style/imgs/stream-icon-${props.mode.type}.png`)} /></NavLink>
+                    <NavLink to='/profile'><img className='circle' src={(user && user.image) ? user.image : require('../style/imgs/user-icon.png')} /></NavLink>
+                </nav></>}
         </>
     )
 }
