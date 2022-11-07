1cb74af19e02ac466177890bbe3f2611f8cb3347
Idan Berger <idanberger22@gmail.com>
2022-11-07 18:44:44 +0200

    minor fixes

--- src/cmps/extention-connect.jsx
+++ src/cmps/extention-connect.jsx
@@ -4,7 +4,7 @@ export function ExtensionConnect({mode}) {
     return(
         <div className={`extension center ${mode.type}`}>
     <img className='extension-img' src={require('../style/imgs/metamask-big.png')} />
-    <p> Metamask wallet support could not be detected, in order to connect your wallet please install it
+    <p> Metamask wallet support could not be detected. in order to connect your wallet please install it
       <a href={isMobile ? "https://play.google.com/store/apps/details?id=io.metamask" : "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en"}
         className='main-color' target="_blank">  here.</a></p>
     <p>Once installed, <span onClick={() => window.location.reload()} className='main-color clickable underline'> refresh the page</span>.</p>
