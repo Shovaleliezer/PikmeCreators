ea68ab91d3cc6fe1a5237daef03b36d56a303954
Idan Berger <idanberger22@gmail.com>
2022-11-13 19:06:06 +0200

    creator checking

--- src/pages/creator/creator-home.jsx
+++ src/pages/creator/creator-home.jsx
@@ -1,12 +1,21 @@
-import { useSelector,useDispatch } from "react-redux"
+import { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { userService } from "../../services/userService"
 import { WalletConnect } from "../../cmps/wallet-connect"
+import { CreatorRegister } from "../../cmps/creator/creator-register"
 import { setIsConnected } from "../../store/reducers/userReducer"
 import { ExtensionConnect } from "../../cmps/extention-connect"
 
 export function CreatorHome() {
     const dispatch = useDispatch()
+    const [isCreator, setIsCreator] = useState(false)
     const { ethereum } = window
     const isConnected = useSelector((state) => state.user.isConnected)
+    const { address } = useSelector((state) => state.user)
+
+    useEffect(() => {
+        if(address) handleCreatorAddress(address)
+    }, [])
 
     if (ethereum) {
         window.ethereum.on('accountsChanged', async (accounts) => {
@@ -16,12 +25,16 @@ export function CreatorHome() {
         })
     }
 
-    const handleCreatorAddress = (address)=>{
-        console.log(address)
+    const handleCreatorAddress = async (address) => {
+        const loadedIsCreator = await userService.checkIsCreator(address)
+        if (loadedIsCreator) setIsCreator(true)
     }
 
     if (!ethereum) return <ExtensionConnect />
-    if (!isConnected) return <WalletConnect from='profile' handleCreatorAddress={handleCreatorAddress}/>
+    if (!isConnected) return <WalletConnect from='profile' handleCreatorAddress={handleCreatorAddress} />
+    if(!isCreator) return <CreatorRegister/>
     
-    return <h1>fsdfsdgsdgdfsg</h1>
+    return (<section className="creator-home">
+        
+    </section>)
 }
