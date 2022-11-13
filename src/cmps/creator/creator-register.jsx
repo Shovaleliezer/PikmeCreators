cb8eef28f8ed9ae547f841825ac06931bfa629b7
Idan Berger <idanberger22@gmail.com>
2022-11-13 19:31:22 +0200

    register start

--- src/cmps/creator/creator-register.jsx
+++ src/cmps/creator/creator-register.jsx
@@ -1,3 +1,12 @@
 export function CreatorRegister(){
-    return <h1>register</h1>
+    return <section className="register">
+        <h1>Create a Nickname</h1>
+        <form>
+            <h3>Nickname</h3>
+            <input type="text" placeholder="Enter your nickname"/>
+            <h3>Image</h3>
+            <label htmlFor='img'>sdfsdfsdf</label>
+            <input id='img' className="non-appear" type="file" placeholder="Upload your image" accept="image/*"/>
+        </form>
+        </section>
 }
