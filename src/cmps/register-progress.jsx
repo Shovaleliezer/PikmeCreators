1b5b52599ed66f1bfeeffec1349a90e4530dddb1
Idan Berger <idanberger22@gmail.com>
2022-11-15 15:27:21 +0200

    progress bar

---
+++ src/cmps/register-progress.jsx
@@ -0,0 +1,24 @@
+export function RegisterProgress({ phase }) {
+    return (<div className='progress'>
+        <div className='main-progress'>
+            <div className="back-main">{phase > 1 ? <span class="material-symbols-outlined">done</span> : 1}</div>
+            <p>Choose your name</p>
+        </div>
+        <div className='mid-progress'>
+            <div className={phase > 1 ? 'back-main' : ''}></div>
+            <p>e</p>
+        </div>
+        <div className='main-progress'>
+            <div className={phase > 1 ? 'back-main' : ''}>2</div>
+            <p>Gaming career</p>
+        </div>
+        <div className='mid-progress'>
+            <div className={phase > 2 ? 'back-main' : ''}></div>
+            <p>e</p>
+        </div>
+        <div className='main-progress'>
+            <div className={phase > 2 ? 'back-main' : ''}>3</div>
+            <p>Gaming experience</p>
+        </div>
+    </div>)
+}
--- src/cmps/register.jsx
+++ src/cmps/register.jsx
@@ -1,12 +1,50 @@
-export function Register(){
+import { useState, useRef } from 'react'
+import { uploadService } from '../services/upload.service.js'
+import { RegisterProgress } from './register-progress.jsx'
+
+export function Register() {
+    const [phase, setPhase] = useState(1)
+    const [creatorDetails, setCreatorDetails] = useState({
+        name: '',
+        img: '',
+        category: '',
+        game: '',
+        region: '',
+        topAchivement: '',
+        status: '',
+        experience: '',
+        social: ''
+    })
+    const nameRef = useRef()
+    const imgRef = useRef()
+    const categoryRef = useRef()
+    const gameRef = useRef()
+    const regionRef = useRef()
+    const topAchivementRef = useRef()
+    const statusRef = useRef()
+    const experienceRef = useRef()
+    const socialRef = useRef()
+
+
+
+    const completePhase1 = async (e) => {
+        e.preventDefault()
+        const uploadedImg = await uploadService.uploadImg(imgRef.current.files[0])
+        setCreatorDetails({ ...creatorDetails, img: uploadedImg.secure_url, name: nameRef.current.value })
+        setPhase(2)
+    }
+
     return <section className="register">
-        <h1>Create a Nickname</h1>
-        <form>
-            <h3>Nickname</h3>
-            <input type="text" placeholder="Enter your nickname"/>
-            <h3>Image</h3>
-            <label htmlFor='img'>sdfsdfsdf</label>
-            <input id='img' className="non-appear" type="file" placeholder="Upload your image" accept="image/*"/>
-        </form>
-        </section>
+        {phase === 1 && <>
+            <h1>Create a Nickname</h1>
+            <form className='phase1' onSubmit={completePhase1}>
+                <h3>Nickname</h3>
+                <input type="text" placeholder="Enter your nickname" required maxLength={15} ref={nameRef} />
+                <h3>Image</h3>
+                <label htmlFor='img'><div className="upload-img"><img src={require('../style/imgs/img-upload.png')} /></div></label>
+                <input id='img' className="non-appear" type="file" placeholder="Upload your image" accept="image/*" required ref={imgRef} />
+                <button>Continue <span class="material-symbols-outlined">arrow_forward</span></button>
+            </form></>}
+            <RegisterProgress phase={phase}/>
+    </section >
 }
