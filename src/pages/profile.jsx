fb8a230007cda4abd41c22df3986795aad325842
Idan Berger <idanberger22@gmail.com>
2022-11-16 18:56:02 +0200

    profile logic

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router"
+import { uploadService } from "../services/upload.service"
 import { getYears } from "../services/utils"
 
 export function Profile() {
     const navigate = useNavigate()
     const user = useSelector((state) => state.user)
 
-    const [img, setImg] = useState({ category: 'gaming', game: 'valorant' })
-    const [selectedCategory, setSelectedCategory] = useState('gaming')
+    const [creator, setCreator] = useState(null)
+    const [img, setImg] = useState('valorant')
     const [isLoader, setIsLoader] = useState(false)
 
     const years = getYears()
 
-    if (!user || !user.creator) {
-        navigate('/')
+    useEffect(() => {
+        if (user && user.creator){
+            setCreator({ ...user.creator, experience: new Date(user.creator.experience).getFullYear() })
+            setImg(user.creator.proficiencyGame)
+        } 
+        else {navigate('/')
         return <></>
+    }
+    }, [])
+
+    const save = () => {
+        console.log(creator)
     }
 
     const copy = () => {
@@ -24,66 +34,56 @@ export function Profile() {
 
     const handleImg = (e) => {
         const { name, value } = e.target
-        if (name === 'category') {
-            if (value === 'gaming') setImg({ category: 'gaming', game: 'valorant' })
-            else setImg({ category: 'sports', game: 'table-tennis' })
-            setSelectedCategory(value)
-        }
-        else setImg({ ...img, [name]: value })
+        setImg(value)
+        handleChange(e)
     }
 
-    const { creator } = user
-    const { nickName, walletAddress, image, category, proficiencyGame, region, topAchievement, status, experience, socialLink } = creator
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setCreator({ ...creator, [name]: value })
+    }
+
+    const handleChangeImage = async (e) => {
+        const uploadedImg = await uploadService.uploadImg(e.target.files[0])
+        setCreator({ ...creator, image: uploadedImg.secure_url })
+    }
+
+    if (!creator) return <></>
+
+    const { nickName, walletAddress, image, proficiencyGame, region, topAchievement, status, experience, socialLink } = creator
     return <section className="profile">
         <h1>Profile</h1>
         <div className="edit-container">
             <div className="h3-wrapper">
                 <h3>Wallet address</h3>
-                <div className="second address"><span>{walletAddress.slice(0,4)+'...' + walletAddress.slice(-4)}</span><img onClick={copy} src={require('../style/imgs/register/address.png')} /></div>
+                <div className="second address"><span>{walletAddress.slice(0, 4) + '...' + walletAddress.slice(-4)}</span><img onClick={copy} src={require('../style/imgs/register/address.png')} /></div>
             </div>
             <div className="h3-wrapper">
                 <h3>Name</h3>
-                <input className="second" type="text" placeholder="edit nickname" maxLength={15} />
+                <input className="second" name='nickName' type="text" placeholder="edit nickname" maxLength={15} value={nickName} onChange={handleChange} />
             </div>
             <div className="main-img-wrapper">
                 <label htmlFor='img'><img className="main-img" src={image} /></label>
-                <input id='img' className="non-appear" type="file" accept="image/*" />
+                <input id='img' className="non-appear" type="file" accept="image/*" onChange={handleChangeImage}/>
             </div>
             <div className='h3-wrapper'>
-                <h3>Category</h3>
-                <div className='img-wrapper second'>
-                    <img src={require(`../style/imgs/register/${img.category}.png`)} />
-                    <select onClick={handleImg} name='category'>
-                        <option value="gaming">Gaming</option>
-                        <option value="sports">Sports</option>
-                    </select>
-                </div>
-            </div>
-            {selectedCategory === 'gaming' ? <div className='h3-wrapper'>
                 <h3>Game</h3>
                 <div className='img-wrapper second'>
-                    <img src={require(`../style/imgs/register/${img.game}.png`)} />
-                    <select onClick={handleImg} name='game'>
-                        <option value="valorant">Valorant</option>
-                        <option value="fifa">Fifa</option>
-                    </select>
-                </div>
-            </div> :
-                <div className='h3-wrapper'>
-                    <h3>sport type</h3>
-                    <div className='img-wrapper second'>
-                        <img src={require(`../style/imgs/register/${img.game}.png`)} />
-                        <select onClick={handleImg} name='game'>
+                    <img src={require(`../style/imgs/register/${img}.png`)} />
+                        <select value={proficiencyGame} onChange={handleImg} name='proficiencyGame'>
                             <option value="table-tennis">Table tennis</option>
                             <option value="poker">Poker</option>
+                            <option value="valorant">Valorant</option>
+                            <option value="fifa">Fifa</option>
                         </select>
-                    </div>
-                </div>}
+                </div>
+            </div>
+
             <div className='h3-wrapper'>
                 <h3>Region</h3>
                 <div className='img-wrapper second'>
                     <img src={require('../style/imgs/register/region.png')} />
-                    <select >
+                    <select name='region' value={region} onChange={handleChange}>
                         <option value="europe">Europe</option>
                         <option value="asia">Asia</option>
                         <option value="south america">South america</option>
@@ -96,7 +96,7 @@ export function Profile() {
                 <h3>Top achivement</h3>
                 <div className='img-wrapper second'>
                     <img src={require('../style/imgs/register/achievement.png')} />
-                    <select >
+                    <select name="topAchievement" value={topAchievement} onChange={handleChange}>
                         <option value="top10">Top 10</option>
                         <option value="top 100">Top 100</option>
                         <option value="top 500">Top 500</option>
@@ -107,24 +107,28 @@ export function Profile() {
                 </div>
             </div>
             <div className='h3-wrapper'>
-                    <h3>Status</h3>
-                    <div className='img-wrapper second'>
-                        <select>
-                            <option value="gaming influencer">Gaming influencer</option>
-                            <option value="streamer">Streamer</option>
-                            <option value="pro player">Pro player</option>
-                        </select>
-                    </div>
+                <h3>Status</h3>
+                <div className='img-wrapper second'>
+                    <select name='status' value={status} onChange={handleChange}>
+                        <option value="gaming influencer">Gaming influencer</option>
+                        <option value="streamer">Streamer</option>
+                        <option value="pro player">Pro player</option>
+                    </select>
                 </div>
-                <div className='h3-wrapper'>
-                    <h3>Experience from</h3>
-                    <div className='img-wrapper second'>
-                        <select >
-                            {years.map(year => <option key={year} value={year}>{year}</option>)}
-                        </select>
-                    </div>
+            </div>
+            <div className='h3-wrapper'>
+                <h3>Experience from</h3>
+                <div className='img-wrapper second'>
+                    <select name='experience' value={experience} onChange={handleChange}>
+                        {years.map(year => <option key={year} value={year}>{year}</option>)}
+                    </select>
                 </div>
+            </div>
+            <div className="h3-wrapper">
+                <h3>Social link</h3>
+                <input className="second" name='socialLink' type="text" placeholder="social link" value={socialLink} onChange={handleChange} />
+            </div>
         </div>
-        <div className="save-wrapper"><button className="save">Continue <span className="material-symbols-outlined">arrow_forward</span></button></div>
+        <div onClick={save} className="save-wrapper"><button className="save">Save <span className="material-symbols-outlined">arrow_forward</span></button></div>
     </section>
 }
