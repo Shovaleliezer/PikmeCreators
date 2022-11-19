9c05f0f805d4defe79168f0dcd1bfe315a57bf6b
idan berger <idanberger22@gmail.com>
2022-11-20 01:54:06 +0200

    create desktop style

--- src/cmps/create.jsx
+++ src/cmps/create.jsx
@@ -1,8 +1,12 @@
-import { useState,useRef } from 'react'
+import { useState, useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { setPopup } from '../store/actions/general.actions'
 
-export function Create(){
+export function Create() {
+    const dispatch = useDispatch()
     const [img, setImg] = useState({ category: 'gaming', game: 'valorant' })
     const [category, setCategory] = useState('gaming')
+    const [isShare, setIsShare] = useState(false)
     const categoryRef = useRef()
     const gameRef = useRef()
     const dateRef = useRef()
@@ -10,11 +14,11 @@ export function Create(){
 
     const handleImg = (e) => {
         const { name, value } = e.target
-        if(name === 'category'){
-            if(value === 'gaming') setImg({ category: 'gaming', game: 'valorant' })
-            else setImg({ category: 'sports', game: 'table-tennis' })
+        if (name === 'category') {
             setCategory(value)
-        } 
+            if (value === 'gaming') setImg({ category: 'gaming', game: 'valorant' })
+            else setImg({ category: 'sports', game: 'table-tennis' })
+        }
         else setImg({ ...img, [name]: value })
     }
 
@@ -24,57 +28,65 @@ export function Create(){
             category: categoryRef.current.value,
             game: gameRef.current.value,
             date: dateRef.current.value,
-            description: descRef.current.value
+            description: descRef.current.value,
+            shareWithCommunity: isShare
         }
         console.log(newEvent)
     }
 
-    return <form className='phase2' onSubmit={addEvent}>
-    <div className='all-select-wrapper'>
-        <div className='h3-wrapper'>
-            <h3>Category</h3>
-            <div className='select-wrapper'>
-                <img src={require(`../style/imgs/register/${img.category}.png`)} />
-                <select ref={categoryRef} onClick={handleImg} name='category'>
-                    <option value="gaming">Gaming</option>
-                    <option value="sports">Sports</option>
-                </select>
-            </div>
+    return <form className='create' onSubmit={addEvent}>
+        <div className='create-upper'>
+            <img src={require('../style/imgs/create-stream.png')} />
+            <h1>Create New Stream</h1>
+            <img src={require('../style/imgs/close-icon.png')} onClick={()=>{dispatch(setPopup(''))}}/>
         </div>
-        {category === 'gaming' ? <div className='h3-wrapper'>
-            <h3>Game</h3>
-            <div className='select-wrapper'>
-                <img src={require(`../style/imgs/register/${img.game}.png`)} />
-                <select ref={gameRef} onClick={handleImg} name='game' required>
-                    <option value="valorant">Valorant</option>
-                    <option value="fifa">Fifa</option>
-                </select>
-            </div>
-        </div> :
+        <div className='all-select-wrapper'>
             <div className='h3-wrapper'>
-                <h3>sport type</h3>
+                <h3>Category</h3>
                 <div className='select-wrapper'>
-                    <img src={require(`../style/imgs/register/${img.game}.png`)} />
-                    <select ref={gameRef} onClick={handleImg} name='game' required>
-                        <option value="table-tennis">Table tennis</option>
-                        <option value="poker">Poker</option>
+                    <img src={require(`../style/imgs/register/${img.category}.png`)} />
+                    <select ref={categoryRef} onChange={handleImg} name='category'>
+                        <option value="gaming">Gaming</option>
+                        <option value="sports">Sports</option>
                     </select>
                 </div>
-            </div>}
-        <div className='h3-wrapper'>
-            <h3>Date</h3>
-            <div className='select-wrapper'>
-            <input type="datetime-local" ref={dateRef} required></input>
+            </div>
+            <div className='h3-wrapper'>
+                <h3>Game</h3>
+                <div className='select-wrapper'>
+                    <img src={require(`../style/imgs/register/${img.game}.png`)} />
+                    <select ref={gameRef} onChange={handleImg} name='game' required>
+                        {category === 'gaming' ? <>
+                            <option value="valorant">Valorant</option>
+                            <option value="fifa">Fifa</option></> :
+                            <>
+                                <option value="table-tennis">Table tennis</option>
+                                <option value="poker">Poker</option>
+                            </>}
+                    </select>
+                </div>
+            </div>
+            <div className='h3-wrapper'>
+                <h3>Date</h3>
+                <div className='select-wrapper'>
+                    <input type="datetime-local" ref={dateRef} required></input>
+                </div>
+            </div>
+            <div className='h3-wrapper'>
+                <h3>Description</h3>
+                <div className='select-wrapper'>
+                    <input type="text" placeholder="insert description" required ref={descRef} />
+                </div>
             </div>
         </div>
-        <div className='h3-wrapper'>
-            <h3>Description</h3>
-            <div className='select-wrapper'>
-            <input type="text" placeholder="event description" required ref={descRef} />
+        <div className='checkbox-wrapper'>
+            <div className='checkbox' onClick={() => setIsShare(!isShare)}>
+                {isShare && <span className="main-color noselect material-symbols-outlined">done</span>}
             </div>
+            <p>Share with community</p>
         </div>
-
-    </div>
-    <button>Create <span className="material-symbols-outlined">arrow_forward</span></button>
-</form>
+        <div className='center'>
+            <button>Create!</button>
+        </div>
+    </form>
 }
