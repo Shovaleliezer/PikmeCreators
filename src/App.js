ea87a42c1e222a0a52e82a0871599cd5f808aa3a
Shovaleliezer <46319026+Shovaleliezer@users.noreply.github.com>
2022-12-15 16:13:15 +0200

    soe

--- src/App.js
+++ src/App.js
@@ -18,53 +18,13 @@ import { UpperPopup } from "../src/cmps/upper-popup"
 
 
 function App() {
-  const { mode, channel, type } = useSelector((storeState) => storeState.generalModule)
-  const ethereum = window.ethereum
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    if (ethereum) {
-      ethereum.on('accountsChanged', async (accounts) => {
-        if (accounts[0]) {
-          const res = await userService.checkIsCreator(accounts[0])
-          if (res) {
-            const loadedCreator = await userService.addCreator(accounts[0], null)
-            if (loadedCreator) dispatch(setCreator(loadedCreator))
-            window.location = '#/'
-          }
-          dispatch(resetState())
-          dispatch(setAddress(accounts[0]))
-          dispatch(setIsConnected(true))
-          window.location = '#/'
-        }
-        else{
-          dispatch(resetState())
-          window.location = '#/'
-        }
-      })
-    }
-  }, [])
 
 
   document.body.classList = [`back-${mode.type}`]
   return (
-    <Router>
-      <div className="app">
-        <Header mode={mode} />
-        <main className='main-layout'>
-          <Routes>
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/confirm/:id' element={<Confirm />} />
-            <Route path='/' element={<Home mode={mode} />} />
-            <Route path='/stream-control' element={<Creator channel={channel} type={type} />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-      <Menu mode={mode} />
-      <Popup mode={mode} />
-      <UpperPopup />
-    </Router>
+   <div>
+    <h1>hello</h1>
+   </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './style/main.scss'
-import { Provider } from 'react-redux'
-import { store } from './store'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    hi
-  </div>
+  
+  <App />
+
 )
