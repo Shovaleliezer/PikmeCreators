f22f418c2345e73a2585d9fac2cc889c0d156d2f
Idan Berger <idanberger22@gmail.com>
2022-12-20 17:55:27 +0200

    pull only

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const app = require('./app')
-const port = 3030;
-const server = http.createServer(app)
-const clients = []	//track connected clients
-const util = require('util')
-
-
-server.listen(port)
