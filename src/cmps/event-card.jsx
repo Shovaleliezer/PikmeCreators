142b2c89ea3e53c203af5e10cb757b20064b78da
Idan Berger <idanberger22@gmail.com>
2022-12-25 12:50:24 +0200

    event card conditions

--- src/cmps/event-card.jsx
+++ src/cmps/event-card.jsx
@@ -1,14 +1,14 @@
 import { useDispatch } from 'react-redux'
 import { NavLink } from 'react-router-dom'
-import { setPopup, setPopupEvent, setUpperPopup,setStreamInfo } from '../store/actions/general.actions'
-import { formatDateHour,getRoute } from '../services/utils'
+import { setPopup, setPopupEvent, setUpperPopup, setStreamInfo } from '../store/actions/general.actions'
+import { formatDateHour, getRoute } from '../services/utils'
 import { eventService } from '../services/event.service'
-export function EventCard({ ev,creator }) {
+export function EventCard({ ev, creator }) {
 
     const dispatch = useDispatch()
 
     const copy = (to) => {
-        if(to==='clients') navigator.clipboard.writeText(getRoute() + ev._id)
+        if (to === 'clients') navigator.clipboard.writeText(getRoute() + ev._id)
         else navigator.clipboard.writeText(getRoute() + 'confirm/' + ev._id)
         dispatch(setUpperPopup('copied'))
     }
@@ -28,18 +28,16 @@ export function EventCard({ ev,creator }) {
             <div className="event-upper">
                 <h3>Event Info</h3>
                 <div>
-                    {/* change to stronger condition ! */}
-                    {((!ev.approved && !ev.over )&& ev.players[0].walletAddress===creator.walletAddress) && <>
+                    {(!ev.approved && ev.players[0].walletAddress === creator.walletAddress) && <>
                         <p onClick={openEdit}>Edit</p>
                         <p onClick={copy}>Share</p>
-                        <p onClick={()=>dispatch(setStreamInfo(ev))}><NavLink to='/stream-control'>Manage</NavLink></p>
                         <p onClick={deleteEvent}>Delete</p>
                     </>}
-                    {(ev.approved && !ev.over ) && <>
-                        <p onClick={()=>{copy('clients')}}>Share</p>
-                        {(ev.players[0].walletAddress===creator.walletAddress) && <p onClick={()=>dispatch(setStreamInfo(ev))}><NavLink to='/stream-control'>Manage</NavLink></p>}
+                    {(ev.approved && !ev.over) && <>
+                        <p onClick={() => { copy('clients') }}>Share</p>
+                        {(ev.players[0].walletAddress === creator.walletAddress) && <p onClick={() => dispatch(setStreamInfo(ev))}><NavLink to='/stream-control'>Manage</NavLink></p>}
                     </>}
-                    {ev.over && <p onClick={deleteEvent}>Delete</p>}
+                    {/* {ev.over && <p onClick={deleteEvent}>Delete</p>} */}
                 </div>
 
             </div>
@@ -57,7 +55,7 @@ export function EventCard({ ev,creator }) {
                     <p>{ev.game}</p>
                     <p>{ev.players.length}</p>
                     <p>{formatDateHour(ev.date)}</p>
-                    <p style={{ color: (ev.over? 'red': (ev.approved ? '#04C300' : '#F37F13'))}}>{(ev.over? 'Over': (ev.approved ? 'approved' : 'waiting'))}</p>
+                    <p style={{ color: (ev.over ? 'red' : (ev.approved ? '#04C300' : '#F37F13')) }}>{(ev.over ? 'Over' : (ev.approved ? 'approved' : 'waiting'))}</p>
                 </div>
             </div>
         </div>
