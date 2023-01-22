c3d45def0e31b38955568b8c54d27e9100cc9532
idanberger22 <idanberger22@gmail.com>
2023-01-22 16:31:41 +0200

    mute and switch mic

--- src/pages/stream.jsx
+++ src/pages/stream.jsx
@@ -48,7 +48,12 @@ function Creator() {
   const [modal, setModal] = useState(false)
   const [isEnd, setIsEnd] = useState(false)
   const [status, setStatus] = useState("not-live")
+  const [cameras, setCameras] = useState([])
   const [cameraIdx, setCameraIdx] = useState(0)
+  const [mics, setMics] = useState([])
+  const [micIdx, setMicIdx] = useState(0)
+  const [isMuted, setIsMuted] = useState(false)
+  const [openOpt, setOpenOpt] = useState('')
   const { streamInfo } = useSelector((storeState) => storeState.generalModule)
   const { viewers } = useSelector((storeState) => storeState.generalModule)
   const { streamPhase } = useSelector((storeState) => storeState.tutorialModule)
@@ -57,11 +62,16 @@ function Creator() {
   let APP_ID = "f4e41c5975dd4a86a326e4c426420ca4"
 
   if (streamPhase === 0) dispatch(setStreamPhase(1))
+  if (!client) {
+    setClient(AgoraRTC.createClient({ mode: "live", codec: "vp8" }))
+  }
 
   useEffect(() => {
     document.documentElement.style.setProperty('--visibility', 'hidden')
     document.body.style.overflow = "hidden"
     if (window.innerWidth < 550) document.querySelector('.main-layout').classList.add("main-stream")
+    loadCameras()
+    loadMics()
     window.screen.orientation.lock('landscape-primary')
 
     return () => {
@@ -91,6 +101,10 @@ function Creator() {
     play()
   }, [cameraIdx])
 
+  useEffect(() => {
+    play('mic')
+  }, [micIdx])
+
   const switchCamera = async () => {
     const cameras = await AgoraRTC.getCameras()
     setCameraIdx(cameraIdx === cameras.length - 1 ? 0 : cameraIdx + 1)
@@ -103,27 +117,54 @@ function Creator() {
     else play()
   }
 
-  const play = async () => {
-    const cameras = await AgoraRTC.getCameras()
-    channelParameters.localVideoTrack.stop()
-    const config = await AgoraRTC.createCameraVideoTrack()
-    if (!cameras[cameraIdx]) {
-      setCameraIdx(0)
-      return
-    }
-    if (cameras[cameraIdx].label.toLowerCase().includes('back')) {
-      document.documentElement.style.setProperty('--video-rotate', '90deg')
-      document.documentElement.style.setProperty('--video-scale', '-1')
+  const play = async (device) => {
+    if (device === 'mic') {
+      const config = await AgoraRTC.createMicrophoneAudioTrack()
+      config.setDevice(mics[micIdx].deviceId)
+      channelParameters.localAudioTrack = config
+      client.unpublish()
+      client.publish([channelParameters.localAudioTrack, channelParameters.localVideoTrack])
+      setIsMuted(false)
     }
     else {
-      document.documentElement.style.setProperty('--video-rotate', '-90deg')
-      document.documentElement.style.setProperty('--video-scale', '1')
+      const cameras = await AgoraRTC.getCameras()
+      channelParameters.localVideoTrack.stop()
+      const config = await AgoraRTC.createCameraVideoTrack()
+      if (!cameras[cameraIdx]) {
+        setCameraIdx(0)
+        return
+      }
+      if (cameras[cameraIdx].label.toLowerCase().includes('back')) {
+        document.documentElement.style.setProperty('--video-rotate', '90deg')
+        document.documentElement.style.setProperty('--video-scale', '-1')
+      }
+      else {
+        document.documentElement.style.setProperty('--video-rotate', '-90deg')
+        document.documentElement.style.setProperty('--video-scale', '1')
+      }
+      config.setDevice(cameras[cameraIdx].deviceId)
+      channelParameters.localVideoTrack = config
+      client.unpublish()
+      client.publish([channelParameters.localAudioTrack, channelParameters.localVideoTrack])
+      channelParameters.localVideoTrack.play("agora_local")
     }
-    config.setDevice(cameras[cameraIdx].deviceId)
-    channelParameters.localVideoTrack = config
+  }
+
+  const loadCameras = async () => {
+    const cameras = await AgoraRTC.getCameras()
+    setCameras(cameras)
+  }
+
+  const loadMics = async () => {
+    const mics = await AgoraRTC.getPlaybackDevices()
+    setMics(mics)
+  }
+
+  const mute = async () => {
+    channelParameters.localAudioTrack = null
     client.unpublish()
     client.publish([channelParameters.localAudioTrack, channelParameters.localVideoTrack])
-    channelParameters.localVideoTrack.play("agora_local")
+    setIsMuted(true)
   }
 
   const endEvent = async () => {
@@ -135,10 +176,6 @@ function Creator() {
       console.log('something went wrong')
     }
     return () => dispatch(setStreamPhase(0))
-  }
-
-  if (!client) {
-    setClient(AgoraRTC.createClient({ mode: "live", codec: "vp8" }))
   }
 
   function initStopOne(client, path) {
@@ -286,18 +323,31 @@ function Creator() {
   try {
     return (<>
       {!isMobile && <div className="stream-container">
-        <div className="settings">
+        <div className="settings noselect">
           <div className="settings-upper">
-            <span className="material-symbols-outlined">settings</span>
-            <p>Settings</p>
-            <span className="material-symbols-outlined hidden">settings</span>
+            <span className="material-symbols-outlined">settings</span><p>Settings</p>
           </div>
+          <div className="option main-color" onClick={() => openOpt === 'camera' ? setOpenOpt('') : setOpenOpt('camera')}>
+            <p>Camera</p><span class="material-symbols-outlined">{openOpt === 'camera' ? 'expand_less' : 'expand_more'}</span></div>
+          {openOpt === 'camera' && cameras.map((camera, idx) => <div className="clickable" onClick={() => setCameraIdx(idx)}>
+            <p className={cameraIdx === idx ? 'sec-color' : ''}>{idx + 1}.{camera.label}</p>
+          </div>)}
+          <div className="option main-color" onClick={() => openOpt === 'mic' ? setOpenOpt('') : setOpenOpt('mic')}>
+            <p>Microphone</p><span class="material-symbols-outlined">{openOpt === 'mic' ? 'expand_less' : 'expand_more'}</span></div>
+          {openOpt === 'mic' && mics.map((mic, idx) => <div className="clickable" onClick={() => setMicIdx(idx)}>
+            <p className={micIdx === idx ? 'sec-color' : ''}>{idx + 1}.{mic.label}</p>
+          </div>)}
         </div>
         <div className="stream">
           <div id="agora_local" className="stream-video"></div>
           <div className="stream-control" style={{ zIndex: streamPhase === 3 ? '1001' : 0 }}>
             <div className="options" style={{ width }}>
-              <img src={require('../style/imgs/stream/mute.png')} />
+              <svg className={`clickable ${isMuted ? 'sec-svg' : ''}`} onClick={() => isMuted ? play('mic') : mute()} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" >
+                <path d="M22.7439 14.4978C22.7439 15.3792 22.597 16.2278 22.3282 17.0186L21.0561 15.7465C21.1394 15.3355 21.1813 14.9172 21.1812 14.4978V12.935C21.1812 12.7278 21.2635 12.5291 21.41 12.3825C21.5566 12.236 21.7553 12.1537 21.9626 12.1537C22.1698 12.1537 22.3685 12.236 22.5151 12.3825C22.6616 12.5291 22.7439 12.7278 22.7439 12.935V14.4978ZM14.9301 20.7489C16.2084 20.7489 17.3961 20.366 18.3869 19.7065L19.509 20.8302C18.3934 21.6392 17.0828 22.137 15.7115 22.2726V25.4372H20.3998C20.607 25.4372 20.8058 25.5196 20.9523 25.6661C21.0988 25.8126 21.1812 26.0114 21.1812 26.2186C21.1812 26.4258 21.0988 26.6246 20.9523 26.7711C20.8058 26.9177 20.607 27 20.3998 27H9.46037C9.25313 27 9.05439 26.9177 8.90785 26.7711C8.76131 26.6246 8.67898 26.4258 8.67898 26.2186C8.67898 26.0114 8.76131 25.8126 8.90785 25.6661C9.05439 25.5196 9.25313 25.4372 9.46037 25.4372H14.1487V22.2726C12.2213 22.0789 10.4346 21.1762 9.13513 19.7396C7.83569 18.303 7.11619 16.4349 7.11621 14.4978V12.935C7.11621 12.7278 7.19854 12.5291 7.34507 12.3825C7.49161 12.236 7.69036 12.1537 7.8976 12.1537C8.10483 12.1537 8.30358 12.236 8.45012 12.3825C8.59666 12.5291 8.67898 12.7278 8.67898 12.935V14.4978C8.67898 16.1557 9.33758 17.7457 10.5099 18.918C11.6822 20.0903 13.2722 20.7489 14.9301 20.7489ZM19.6184 6.68395V14.3087L18.0556 12.7459V6.68395C18.0592 5.86859 17.7441 5.08409 17.1774 4.49784C16.6107 3.91159 15.8374 3.56999 15.0223 3.54593C14.2073 3.52187 13.4152 3.81725 12.8149 4.36905C12.2146 4.92085 11.8537 5.68539 11.8092 6.49954L10.4887 5.179C10.8465 4.12763 11.5657 3.23734 12.5185 2.66663C13.4712 2.09592 14.5955 1.88183 15.6913 2.06246C16.7871 2.24309 17.7832 2.80673 18.5024 3.65303C19.2215 4.49933 19.617 5.57337 19.6184 6.68395Z"
+                  fill='white' fill-opacity="0.9" />
+                <path d="M17.2521 18.5722L16.0832 17.4032C15.6091 17.5914 15.0961 17.6608 14.589 17.6051C14.0819 17.5495 13.5962 17.3706 13.1742 17.0841C12.7521 16.7975 12.4067 16.412 12.1679 15.9613C11.9291 15.5105 11.8043 15.0081 11.8043 14.498V13.1259L10.2415 11.5631V14.498C10.2413 15.319 10.4566 16.1256 10.866 16.8372C11.2753 17.5488 11.8644 18.1404 12.5742 18.5529C13.284 18.9654 14.0896 19.1843 14.9106 19.1877C15.7315 19.191 16.539 18.9788 17.2521 18.5722ZM5 4.11181L23.7533 22.8651L24.8597 21.7586L6.10644 3.00537L5 4.11181Z"
+                  fill='white' fill-opacity="0.9" />
+              </svg>
               <img onClick={() => { (status == "live") ? setModal('exit') : initStopOne(client) }} src={require('../style/imgs/stream/home.png')} />
             </div>
             <div className="start">
@@ -346,7 +396,12 @@ function Creator() {
             <NavLink to='/'><img className="smaller" src={require('../style/imgs/stream/home.png')} /></NavLink>
             {status != "live" ? <img onClick={() => { hasStarted() ? setModal('start') : dispatch(setStreamPopup(timeUntilEvent)) }} src={require('../style/imgs/stream/start.png')} />
               : <img onClick={() => setModal('end')} src={require('../style/imgs/stream/pause.png')} />}
-            <img className="smaller" src={require('../style/imgs/stream/mute.png')} />
+            <svg className={`clickable ${isMuted ? 'sec-svg' : ''}`} onClick={() => isMuted ? play('mic') : mute()} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" >
+              <path d="M22.7439 14.4978C22.7439 15.3792 22.597 16.2278 22.3282 17.0186L21.0561 15.7465C21.1394 15.3355 21.1813 14.9172 21.1812 14.4978V12.935C21.1812 12.7278 21.2635 12.5291 21.41 12.3825C21.5566 12.236 21.7553 12.1537 21.9626 12.1537C22.1698 12.1537 22.3685 12.236 22.5151 12.3825C22.6616 12.5291 22.7439 12.7278 22.7439 12.935V14.4978ZM14.9301 20.7489C16.2084 20.7489 17.3961 20.366 18.3869 19.7065L19.509 20.8302C18.3934 21.6392 17.0828 22.137 15.7115 22.2726V25.4372H20.3998C20.607 25.4372 20.8058 25.5196 20.9523 25.6661C21.0988 25.8126 21.1812 26.0114 21.1812 26.2186C21.1812 26.4258 21.0988 26.6246 20.9523 26.7711C20.8058 26.9177 20.607 27 20.3998 27H9.46037C9.25313 27 9.05439 26.9177 8.90785 26.7711C8.76131 26.6246 8.67898 26.4258 8.67898 26.2186C8.67898 26.0114 8.76131 25.8126 8.90785 25.6661C9.05439 25.5196 9.25313 25.4372 9.46037 25.4372H14.1487V22.2726C12.2213 22.0789 10.4346 21.1762 9.13513 19.7396C7.83569 18.303 7.11619 16.4349 7.11621 14.4978V12.935C7.11621 12.7278 7.19854 12.5291 7.34507 12.3825C7.49161 12.236 7.69036 12.1537 7.8976 12.1537C8.10483 12.1537 8.30358 12.236 8.45012 12.3825C8.59666 12.5291 8.67898 12.7278 8.67898 12.935V14.4978C8.67898 16.1557 9.33758 17.7457 10.5099 18.918C11.6822 20.0903 13.2722 20.7489 14.9301 20.7489ZM19.6184 6.68395V14.3087L18.0556 12.7459V6.68395C18.0592 5.86859 17.7441 5.08409 17.1774 4.49784C16.6107 3.91159 15.8374 3.56999 15.0223 3.54593C14.2073 3.52187 13.4152 3.81725 12.8149 4.36905C12.2146 4.92085 11.8537 5.68539 11.8092 6.49954L10.4887 5.179C10.8465 4.12763 11.5657 3.23734 12.5185 2.66663C13.4712 2.09592 14.5955 1.88183 15.6913 2.06246C16.7871 2.24309 17.7832 2.80673 18.5024 3.65303C19.2215 4.49933 19.617 5.57337 19.6184 6.68395Z"
+                fill='white' fill-opacity="0.9" />
+              <path d="M17.2521 18.5722L16.0832 17.4032C15.6091 17.5914 15.0961 17.6608 14.589 17.6051C14.0819 17.5495 13.5962 17.3706 13.1742 17.0841C12.7521 16.7975 12.4067 16.412 12.1679 15.9613C11.9291 15.5105 11.8043 15.0081 11.8043 14.498V13.1259L10.2415 11.5631V14.498C10.2413 15.319 10.4566 16.1256 10.866 16.8372C11.2753 17.5488 11.8644 18.1404 12.5742 18.5529C13.284 18.9654 14.0896 19.1843 14.9106 19.1877C15.7315 19.191 16.539 18.9788 17.2521 18.5722ZM5 4.11181L23.7533 22.8651L24.8597 21.7586L6.10644 3.00537L5 4.11181Z"
+                fill='white' fill-opacity="0.9" />
+            </svg>
             <img className="smaller" src={require('../style/imgs/stream/settings.png')} />
           </div>
         </section>
