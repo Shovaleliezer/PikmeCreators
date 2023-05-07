045d6374e9210aaec06462338eb32281a229c0d0
idanberger22 <idanberger22@gmail.com>
2023-05-07 18:31:37 +0300

    payments

--- src/cmps/edit.jsx
+++ src/cmps/edit.jsx
@@ -59,7 +59,6 @@ export function Edit() {
 
     const handleFund = (ev) => {
         const { name, value } = ev.target
-        console.log(name, value)
         setFund({ ...fund, [name]: value })
     }
 
--- src/pages/control-panel.jsx
+++ src/pages/control-panel.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react"
-import { useSelector, useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
 import { useNavigate } from "react-router"
 import { adminService } from "../services/admin.service"
 import { Error } from './error'
 import { ControlWaiting } from "../cmps/control-waiting"
 import { ControlCurrent } from "../cmps/control-current"
+import { ControlPayment } from "../cmps/control-payment"
 
 export function ControlPanel() {
     const navigate = useNavigate()
     const [isAdmin, setIsAdmin] = useState('loading')
     const [opt, setOpt] = useState('waiting list')
     const user = useSelector((state) => state.user)
-    const options = ['waiting list', 'current events', 'payment', 'history']
+    const options = ['waiting list', 'upcoming', 'payment', 'history']
 
     useEffect(() => {
         authorize()
@@ -19,35 +20,35 @@ export function ControlPanel() {
     }, [])
 
     const authorize = async () => {
-            try {
-                console.log(user.creator._id)
-                const admin = await adminService.authorize(user.creator._id)
-                if(admin) setIsAdmin(true)
-                else setIsAdmin(false)
-            }
+        try {
+            const admin = await adminService.authorize(user.creator._id)
+            if (admin) setIsAdmin(true)
+            else setIsAdmin(false)
+        }
 
-            catch {
-                setIsAdmin(false)
-            }
+        catch {
+            setIsAdmin(false)
+        }
     }
 
     if (isAdmin === 'loading') return <div className="home"><div className="home"><div className="loader"><div></div><div></div><div></div><div></div>
-    <div></div><div></div><div></div><div></div></div></div></div>
+        <div></div><div></div><div></div><div></div></div></div></div>
 
-    if(!isAdmin){
+    if (!isAdmin) {
         navigate('/')
         return <h1>Unauthorized</h1>
-    } 
+    }
 
     try {
         return (
             <section className="control">
                 <div className="control-banner"><h1>Wellcome {user.creator.nickName}</h1></div>
                 <div className="options-bar">
-                    {options.map(op => <p className={op === opt ? 'chosen' : ''} key={op} onClick={() => setOpt(op)}>{op}</p>)}
+                    {options.map(op => <p className={op === opt ? 'chosen' : ''} key={op} onClick={() => setOpt(op)}>{op.charAt(0).toUpperCase() + op.slice(1)}</p>)}
                 </div>
                 {opt === 'waiting list' && <ControlWaiting />}
-                {opt === 'current events' && <ControlCurrent/>}
+                {opt === 'upcoming' && <ControlCurrent />}
+                {opt === 'payment' && <ControlPayment />}
             </section>)
     }
     catch {
